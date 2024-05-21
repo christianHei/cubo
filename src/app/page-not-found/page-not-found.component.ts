59175ce6d8945ba7248aa8dbdf20f9f79628eb06
christian.juina <christian.juina@cce.gob.ec>
2024-05-21 11:11:36 -0500

    page not found

---
+++ src/app/page-not-found/page-not-found.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page-not-found',
+  template: `<h1>Página No Encontrada</h1>`,
+})
+export class PageNotFoundComponent { }
